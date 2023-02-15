ff509bb328bfe0a5c783b3f264e0deeecb74482c
dineshkumarnagarajan <97437443+dineshkumarnagarajan@users.noreply.github.com>
2023-02-15 18:55:26 +0530

    Update normalActions.js

--- src/actions/normalActions.js
+++ src/actions/normalActions.js
@@ -1,12 +1,4 @@
-export function storeCapacityCountRow(data, expand) {
-  return {
-    type: "STORE_CAPACITY_COUNT_ROW",
-    payload: {
-      data,
-      expand,
-    },
-  };
-}
+
 
 export function changeTheme(payload) {
   return { type: "CHANGE_THEME", payload };
